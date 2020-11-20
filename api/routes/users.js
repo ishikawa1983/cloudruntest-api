0e4db3911e83cc51b1a978ea1022f879e06c266e
ishikawa1983 <ishikawa1983@gmail.com>
2020-11-20 14:11:07 +0900

    test commit 2

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('user respond with a resource');
+  res.send('respond with a user resource');
 });
 
 module.exports = router;
