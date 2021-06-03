b433d0445517806eab9f8b5f77feef84a4de7a14
ishikawa1983 <ishikawa1983@gmail.com>
2021-06-03 18:23:46 +0900

    javac

---
+++ javac/routes/compile.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const fs = require("fs");
+const encoding = require('encoding-japanese');
+const router = express.Router();
+
+const toString = (bytes) => {
+  return encoding.convert(bytes, {
+    from: 'SJIS',
+    to: 'UNICODE',
+    type: 'string',
+  });
+};
+
+router.post('/', function(req, res, next) {
+  try {
+    fs.writeFileSync("JavaTest.java", req.body.code);
+    console.log('write end');
+  } catch(e) {
+    console.log(e);
+  }
+  var cmd = 'javac JavaTest.java';
+  var ret = {};
+  require('child_process').exec(cmd, { encoding: 'Shift_JIS' }, (err, stdout, stderr) => {
+    if (stderr) {
+      ret.error = toString(stderr);
+      res.send(JSON.stringify(ret));
+    } else {
+      cmd = 'java JavaTest';
+      require('child_process').exec(cmd, (err, stdout, stderr) => {
+        ret.output = stdout;
+        res.send(JSON.stringify(ret));
+      })
+    }
+  })
+});
+
+module.exports = router;
